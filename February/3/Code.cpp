55c17b6bab9b91ecb910ef1f2a9cd81ada8172f5
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-02-03 23:26:56 +0530

    3rd February 📅 GFG-POTD️‍🔥

---
+++ February/3/Code.cpp
@@ -0,0 +1,94 @@
+
+// Program to check if linked list is empty or not
+#include<iostream>
+using namespace std;
+const long long unsigned int MOD = 1000000007;
+
+/* Link list Node */
+struct Node
+{
+    bool data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Below global variable is declared in code for modulo arithmetic
+const long long unsigned int MOD = 1000000007; */
+
+/* Link list Node/
+struct Node
+{
+    bool data;   // NOTE data is bool
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+class Solution
+{
+    public:
+    const long long unsigned int MOD = 1000000007;
+
+// Function to return decimal equivalent modulo 1000000007 of binary linked list
+long long unsigned int decimalValue(Node *head) {
+    long long unsigned int ans = 0;
+    
+    // Traverse the linked list and calculate decimal equivalent
+    while (head) {
+        ans = (ans * 2 + head->data) % MOD;
+        head = head->next;
+    }
+    
+    return ans;
+}
+
+};
+
+
+
+
+void append(struct Node** head_ref, struct Node **tail_ref, bool new_data)
+{
+
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isEmpty(struct Node *head);
+
+int main()
+{
+    bool l;
+    int i, n, T;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        Solution obj;
+        cout << obj.decimalValue(head) << endl;
+    }
+    return 0;
+}
