ec643bd0bb699ad058bc0716a6268168b6815568
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-03-01 23:36:20 +0530

    1st March 📅 GFG-POTD️‍🔥

---
+++ March/1/Code.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+       int start = 0;
+       int end = n-1;
+       while(start<=end){
+           int mid = (start+end)/2;
+           int prev = mid-1>=0 ? mid-1 : 0;
+           int next = mid+1<n ? mid+1 : n-1;
+           if(arr[mid] >= arr[next] && arr[mid] >= arr[prev]) {
+                return mid;
+            }
+           else if(arr[mid]<arr[prev]){
+               end=mid-1;
+           }
+           else{
+               start = mid+1;
+           }
+       }
+       return -1;
+    }
+};
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
